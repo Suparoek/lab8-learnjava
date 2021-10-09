f4fedac63aefb0bbcf6e748824ea2ae177d33dee
Suparoek <39317424+Suparoek@users.noreply.github.com>
2021-10-09 14:18:16 +0700

    lab8

---
+++ src/com/Labeee/lab8Learnjava/Lab8Learnjava.java
@@ -0,0 +1,14 @@
+package com.Labeee.lab8Learnjava;
+
+public class Lab8Learnjava {
+	public static void main(String[] arges) {
+		for(int i=0;i<=20;i++) {
+			if(i==11) {
+				System.out.println("i="+i+"จบการทำงาน");
+				break;
+			}
+			System.out.println("รอบที่ i="+i);
+		}
+	}
+
+}
